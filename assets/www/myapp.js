700be53320f6c2c40a539ab4fac5800074f4433f
Felipe Mascarenhas <felipemascarenhas@gmail.com>
2014-04-01 21:03:17 -0300

    Atualizado

---
+++ assets/www/myapp.js
@@ -0,0 +1,36 @@
+ function DOMLoaded() {
+	 
+	 document.addEventListener("deviceready", phonegapLoaded, false);
+	 
+ }
+ 
+ 
+ function phonegapLoaded() {
+	
+	$(function () {
+		$("body").append("<h1>Device is Ready </h1>");
+		
+	}); 
+	 
+ }
+ 
+ 
+ function alert() {
+	 
+	 navigator.notification.alert(
+	 	'Conteudo Vazio', 
+		dismissed,
+		'A Grande Sacada',
+		'Done'  
+	 );
+	 
+ }
+ 
+ function dismissed() {
+
+	$(function() {
+		$("body").append("<h1>DISMISSED</h1>");
+		
+		 });	 
+	 
+ }
